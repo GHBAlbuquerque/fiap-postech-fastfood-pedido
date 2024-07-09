ea00c5cb8513895652b2c8e5963b5927196a42ba
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2024-07-09 17:54:55 -0300

    deploy: ajuste nas configs do docker build e no arquivo dockerfile

--- src/main/java/com/fiap/fastfood/common/beans/AWSDynamoBeanDeclaration.java
+++ src/main/java/com/fiap/fastfood/common/beans/AWSDynamoBeanDeclaration.java
@@ -1,10 +1,7 @@
 package com.fiap.fastfood.common.beans;
 
-import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
-import com.amazonaws.services.securitytoken.AWSSecurityTokenService;
-import com.amazonaws.services.securitytoken.AWSSecurityTokenServiceClientBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.regions.Region;
@@ -13,17 +10,8 @@ import software.amazon.awssdk.regions.Region;
 public class AWSDynamoBeanDeclaration {
     @Bean
     public AmazonDynamoDB amazonDynamoDB() {
-//        AWSSecurityTokenService stsClient =
-//                AWSSecurityTokenServiceClientBuilder.standard()
-//                        .withCredentials(new DefaultAWSCredentialsProviderChain())
-//                        .withRegion(String.valueOf(Region.US_EAST_1))
-//                        .build();
-
         return AmazonDynamoDBClient.builder()
-                .withCredentials(new DefaultAWSCredentialsProviderChain())
                 .withRegion(String.valueOf(Region.US_EAST_1))
                 .build();
     }
-
-
 }
