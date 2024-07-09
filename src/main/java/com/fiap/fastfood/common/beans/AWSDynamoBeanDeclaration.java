54dc26a115ac8e926203a139a9c6db1f7bf5a16d
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2024-07-09 15:28:34 -0300

    chore: deleta properties local

--- src/main/java/com/fiap/fastfood/common/beans/AWSDynamoBeanDeclaration.java
+++ src/main/java/com/fiap/fastfood/common/beans/AWSDynamoBeanDeclaration.java
@@ -1,17 +1,26 @@
 package com.fiap.fastfood.common.beans;
 
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.securitytoken.AWSSecurityTokenService;
+import com.amazonaws.services.securitytoken.AWSSecurityTokenServiceClientBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.regions.Region;
 
 @Configuration
 public class AWSDynamoBeanDeclaration {
-
     @Bean
     public AmazonDynamoDB amazonDynamoDB() {
+//        AWSSecurityTokenService stsClient =
+//                AWSSecurityTokenServiceClientBuilder.standard()
+//                        .withCredentials(new DefaultAWSCredentialsProviderChain())
+//                        .withRegion(String.valueOf(Region.US_EAST_1))
+//                        .build();
+
         return AmazonDynamoDBClient.builder()
+                .withCredentials(new DefaultAWSCredentialsProviderChain())
                 .withRegion(String.valueOf(Region.US_EAST_1))
                 .build();
     }
