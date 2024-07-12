cecebad549b1e45805082d7d96823b95db4114b1
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2024-07-12 10:25:12 -0300

    test: impl do cucumber - WIP

---
+++ src/test/java/com/fiap/fastfood/bdd/StepDefinition.java
@@ -0,0 +1,47 @@
+package com.fiap.fastfood.bdd;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class StepDefinition {
+
+    @Given("a new valid order request")
+    public void a_new_valid_order_request() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("has an item list with valid products")
+    public void has_an_item_list_with_valid_products() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("a new order is succesfully created")
+    public void a_new_order_is_succesfully_created() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+    @Given("a new invalid order request")
+    public void a_new_invalid_order_request() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("has an item with non-existant product")
+    public void has_an_item_with_non_existant_product() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("an error is throwed")
+    public void an_error_is_throwed() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+}
